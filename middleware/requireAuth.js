c42b48334359b004c371067bbbed17b29cf5dcd5
Mukhil <mukhil1212@gmail.com>
2024-08-20 16:17:56 +0530

    Secreat changes

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -11,7 +11,7 @@ const requireAuth=async(req,res,next)=>{
     
     //verify token
     try{
-        const {_id}=jwt.verify(token,process.env.SECRET)
+        const {_id}=jwt.verify(token,process.env.Secret)
         req.user=await User.findOne({_id}).select('_id');
         next()
     }catch(error){
